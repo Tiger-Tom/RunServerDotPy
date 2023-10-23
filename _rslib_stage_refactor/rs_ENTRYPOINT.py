eaf36d90011f4faa7f108b417b2501388d4bb980
Shae <a.thomas.vc@gmail.com>
2023-10-23 11:43:42 -0700

    Removed not-yet added modules from rs_ENTRYPOINT

--- _rslib_stage_refactor/rs_ENTRYPOINT.py
+++ _rslib_stage_refactor/rs_ENTRYPOINT.py
@@ -4,42 +4,20 @@
 
 #> Imports
 from _rslib import helper_types
-from _rslib.rschatcommand import ChatCommands
-from _rslib.rsconfig import Config
-from _rslib.rsio import RSIO
-from _rslib.rsmcserv import MCServerLang, MCServerOutputParser, UserManager, Tellraw
-from _rslib.rschatcommand import ChatCommands
-from _rslib.rsplugins import PluginManager
 #</Imports
 
 #> Header
 class RunServer:
     __slots__ = (
         'logger',
-        'HelperTypes', 'HT',
         'Bootstrapper', 'BS',
-        'Config', 'C',
-        'IO',
-        'Lang', 'L',
-        'Parser', 'Pa',
-        'Users', 'U',
-        'Tellraw', 'TR',
-        'ChatCommands', 'CC',
-        'Plugins', 'P',
+        'HelperTypes', 'HT',
     )
     def __init__(self, bs: 'Bootstrapper'):
         self.logger = bs.root_logger
         self.logger.info('Initializing entrypoint')
-        self.HelperTypes = self.HT = helper_types
         self.Bootstrapper = self.BS = bs
-        self.Config = self.C = Config(self)
-        self.IO = RSIO(self)
-        self.Lang = self.L = MCServerLang(self)
-        self.Parser = self.Pa = MCServerOutputParser(self)
-        self.Users = self.U = UserManager(self)
-        self.Tellraw = self.TR = Tellraw(self)
-        self.ChatCommands = self.CC = ChatCommands(self)
-        self.Plugins = self.P = PluginManager(self)
+        self.HelperTypes = self.HT = helper_types
     def __calL__(self):
         self.logger.info('Entrypoint starting')
 #</Header
