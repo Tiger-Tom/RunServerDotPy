cf37f639e8985964b44f2da1085eec71762fd867
Shae <a.thomas.vc@gmail.com>
2023-10-24 12:54:27 -0700

    Augmented SimpleContainer to support assignment-on-creation

--- _rsruntime/lib/types/simplecontainer.py
+++ _rsruntime/lib/types/simplecontainer.py
@@ -7,4 +7,7 @@
 def SimpleContainer(*names):
     class SimpleContainer:
         __slots__ = names
+        def __init__(self, **vals):
+            for k,v in vals.items():
+                setattr(self, k, v)
     return SimpleContainer
