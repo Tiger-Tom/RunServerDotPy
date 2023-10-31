223b195980fb3a2fd7582658ad12602e52491693
Shae <a.thomas.vc@gmail.com>
2023-10-31 15:33:03 -0700

    Mostly rewrote mkmanifest.py devel script

--- devel/mkmanifest.py
+++ devel/mkmanifest.py
@@ -1,34 +1,52 @@
 #!/bin/python3
 
 #> Imports
+import argparse
 import sys
 from pathlib import Path
 import hashlib
 import json
 import subprocess
+from pprint import pprint
 #</Imports
 
 #> Main >/
 eprint = lambda *k,**kw: print(*k, **kw, file=sys.stderr)
-bmeta = lambda n,mu,fu: {'_metadata': {'name': n, 'manifest_upstream': mu, 'file_upstream': fu}}
 
-match sys.argv:
-    case [_, 'add', name, path, mupstream, fupstream]:
-        path = Path(path) # path path path
-        man = bmeta(name, mupstream, fupstream)
-        for p in path.glob('**/*.py'):
-            eprint(f'Adding manifest entry for {p.name}')
-            with p.open('rb') as f:
-                hd = hashlib.file_digest(f, hashlib.sha1).hexdigest()
-            eprint(f'"{p.name}": "{hd}"')
-            man[p.relative_to(path).as_posix()] = hd
-        print(json.dumps(man, indent=4))
-    case [_, 'update', path]:
-        path = Path(path)
-        with path.open('r') as f:
-            cman = json.load(f)
+def parse_args(args=None):
+    p = argparse.ArgumentParser(prog='mkmanifest.py')
+    sp = p.add_subparsers(dest='cmd')
+    u = sp.add_parser('update')
+    u.add_argument('old_manifest', help='The manifest to read old values from', type=Path)
+    u.add_argument('--output', help='The file to write to (defaults to stdout)', type=Path, default=None)
+    a = sp.add_parser('add')
+    a.add_argument('name', help='The name of the manifest')
+    a.add_argument('path', help='The path to create a manifest for', type=Path)
+    a.add_argument('manifest_upstream', help='The URI to fetch manifest updates from', nargs='?')
+    a.add_argument('file_upstream', help='The URI to fetch file updates from', nargs='?')
+    a.add_argument('--output', help='The file to write to (defaults to stdout)', type=argparse.FileType('w'), default=sys.stdout)
+    args = p.parse_args(args)
+    if args.cmd == 'update':
+        with args.old_manifest.open('r') as f:
+            old = json.load(f)
         eprint('Fetched old manifest')
-        subprocess.call((sys.argv[0], 'add', cman['_metadata']['name'], str(path.parent), cman['_metadata']['manifest_upstream'], cman['_metadata']['file_upstream']))
-    case _:
-        eprint('usage: mkmanifest.py add [name] [path] [manifest upstream] [file upstream]\nor:    mkmanifest.py update [path]')
-        exit(1)
+        parse_args(('add', old['_metadata']['name'], str(args.old_manifest.parent), old['_metadata']['manifest_upstream'], old['_metadata']['file_upstream']) \
+                   +(('--output', str(args.output)) if args.output is not None else ()))
+        return
+    if args.cmd != 'add': raise RuntimeError(args.cmd, args)
+    manifest = {
+        '_metadata': {
+            'name': args.name,
+            'manifest_upstream': args.manifest_upstream,
+            'file_upstream': args.file_upstream,
+        },
+    }
+    for p in sorted(set(args.path.glob('**/*.py')) | set(args.path.glob('**/rs_*.*'))):
+        if p.suffix in {'.pyc'}: continue
+        eprint(f'Adding manifest entry for {p.name}')
+        with p.open('rb') as f:
+            hd = hashlib.file_digest(f, hashlib.sha1).hexdigest()
+        eprint(f'"{p.name}": "{hd}"')
+        manifest[p.relative_to(args.path).as_posix()] = hd
+    json.dump(manifest, args.output, indent=4, sort_keys=False)
+if __name__ == '__main__': parse_args()
