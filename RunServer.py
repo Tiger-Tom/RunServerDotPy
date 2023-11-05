b25dda8b877be3b49fd85520b21f7bbd391a5a18
Shae <a.thomas.vc@gmail.com>
2023-11-04 17:44:44 -0700

    Updated logger format; shortened default stderr logger format; added --verbose-log-headers argument

--- RunServer.py
+++ RunServer.py
@@ -9,17 +9,26 @@ from pathlib import Path
 #> Logging
 def setup_logger():
     log_path = (Path.cwd() / '_rslog'); log_path.mkdir(exist_ok=True)
-    log_fmt = '[%(asctime)s] [%(name)s<%(funcName)s>:%(levelname)s] %(message)s'
+    date_fmt_short = '%H:%M:%S'
+    date_fmt_long = '%Y-%m-%d %H:%M:%S'
+    log_fmt_short = '[$asctime] [$name/$threadName/$levelname] $message'
+    log_fmt_long = '[$asctime] [$name/$processName:$threadName<$module.$funcName[$lineno]>/$levelname] $message'
     logging.basicConfig(
         filename=log_path / 'RunServer.log',
         level=logging.DEBUG if '--debug' in sys.argv[1:] \
             else logging.INFO if '--verbose' in sys.argv[1:] \
             else logging.WARNING,
-        format=log_fmt,
+        format=log_fmt_long,
+        datefmt=date_fmt_long,
+        style='$'
     )
     logger = logging.getLogger()
     stream_h = logging.StreamHandler()
-    stream_h.setFormatter(logging.Formatter(log_fmt))
+    stream_h.setFormatter(logging.Formatter(
+        log_fmt_long if '--verbose-log-headers' in sys.argv[1:] else log_fmt_short,
+        date_fmt_long if '--verbose-log-headers' in sys.argv[1:] else date_fmt_short,
+        style='$',
+    ))
     logger.addHandler(stream_h)
 #</Logging
 
