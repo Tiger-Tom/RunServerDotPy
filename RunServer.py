3fad3cebdaac29d9730fcb07b1afdc693143df49
Shae <a.thomas.vc@gmail.com>
2023-11-04 18:00:12 -0700

    Organized logging setup

--- RunServer.py
+++ RunServer.py
@@ -9,10 +9,13 @@ from pathlib import Path
 #> Logging
 def setup_logger():
     log_path = (Path.cwd() / '_rslog'); log_path.mkdir(exist_ok=True)
-    date_fmt_short = '%H:%M:%S'
-    date_fmt_long = '%Y-%m-%d %H:%M:%S'
+    # Formats
     log_fmt_short = '[$asctime] [$name/$threadName/$levelname] $message'
     log_fmt_long = '[$asctime] [$name/$processName:$threadName<$module.$funcName[$lineno]>/$levelname] $message'
+    ## Date
+    date_fmt_short = '%H:%M:%S'
+    date_fmt_long = '%Y-%m-%d %H:%M:%S'
+    # Configure logger
     logging.basicConfig(
         filename=log_path / 'RunServer.log',
         level=logging.DEBUG if '--debug' in sys.argv[1:] \
@@ -22,14 +25,18 @@ def setup_logger():
         datefmt=date_fmt_long,
         style='$'
     )
+    # Create logger
     logger = logging.getLogger()
+    # Stream handler
     stream_h = logging.StreamHandler()
+    logger.addHandler(stream_h)
+    ## Format stream handler
     stream_h.setFormatter(logging.Formatter(
         log_fmt_long if '--verbose-log-headers' in sys.argv[1:] else log_fmt_short,
         date_fmt_long if '--verbose-log-headers' in sys.argv[1:] else date_fmt_short,
         style='$',
     ))
-    logger.addHandler(stream_h)
+    
 #</Logging
 
 #> Bootstrap >/
