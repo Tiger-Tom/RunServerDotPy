3b8480e153f2a477913361d3019033a53aa4d9ac
Shae <a.thomas.vc@gmail.com>
2023-11-08 14:06:10 -0800

    Changed how manifests are "compiled" to bytearrays

--- devel/mkmanifest.py
+++ devel/mkmanifest.py
@@ -51,15 +51,19 @@ def parse_args(args=None):
             'signature': None,
         },
     }
-    de = sys.getdefaultencoding()
-    ba = bytearray(
-        bytes((255,)) if args.name is None else bytes(args.name, de) + \
-        bytes(1) + \
-        bytes((255,)) if args.manifest_upstream is None else bytes(args.manifest_upstream, de) + \
-        bytes(1) + \
-        bytes((255,)) if args.file_upstream is None else bytes(args.file_upstream, de) + \
-        bytes(2)
-    )
+    ba = bytearray()
+    # metadata
+    ba = bytearray()
+    if args.name is None: ba.append(255)
+    else: ba.extend(args.name.encode())
+    ba.append(0)
+    if args.manifest_upstream is None: ba.append(255)
+    else: ba.extend(args.manifest_upstream.encode())
+    ba.append(0)
+    if args.file_upstream is None: ba.append(255)
+    else: ba.extend(args.file_upstream.encode())
+    ba.extend((0, 0))
+    # file hashes
     for p in sorted(set(args.path.glob('**/*.py')) | set(args.path.glob('**/rs_*.*'))):
         if p.suffix in {'.pyc'}: continue
         eprint(f'Adding manifest entry for {p.name}')
@@ -71,6 +75,7 @@ def parse_args(args=None):
         ba.append(255)
         ba.extend(p.relative_to(args.path).as_posix().encode())
         ba.append(0)
+    eprint(ba)
     eprint(f'Signing with privkey {base64.b85encode(priv.private_bytes_raw()).decode()}')
     manifest['_metadata']['signature'] = pysign.signstr(priv, bytes(ba))
     eprint(f'Signature: {manifest["_metadata"]["signature"]}')
