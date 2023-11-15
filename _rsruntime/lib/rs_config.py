246f28d160adc05f1eeb711d6fe1573eda5d1522
Shae <a.thomas.vc@gmail.com>
2023-11-15 08:54:16 -0800

    Added 'dict_vals' optional mode to Config mass_set_default mode

--- _rsruntime/lib/rs_config.py
+++ _rsruntime/lib/rs_config.py
@@ -2,6 +2,7 @@
 
 #> Imports
 from pathlib import Path
+import warnings
 #</Imports
 
 # RunServer Module
@@ -20,7 +21,29 @@ class Config(FileBackedDict):
         super().__init__(self.conf_path, 60.0)
         self.start_autosync()
         RS.BS.register_onclose(self.close)
-    def mass_set_default(self, pfx: str | None = None, **values):
+    def mass_set_default(self, pfx: str | None = None, dict_vals: dict[str, ...] = None, **values):
+        '''
+            Sets a large amount of default values
+                When pfx is not None, it is prepended (with a / if it doesn't already have one) to each key
+            Values are either given through dict_vals or **values (keyword args)
+                Using both is probably bad but not prohibited
+                    A SyntaxWarning shall be issued upon you to remind you of your choices. 
+                If a value is in both:
+                    if it is the same in both, a UserWarning is issued
+                    if it is not the same in both, ValueError is raised
+                    Once this has been checked, they are merged together
+            If a total of 0 values are given, an error is logged
+            If a total of 1 value is given, a warning is logged
+            Otherwise, an infop is logged decribing how many keys will be set
+        '''
+        if dict_vals.keys() and values.keys():
+            warnings.warn('Using both dict_vals and values is a bad practice', SyntaxWarning)
+        for k in dict_vals.keys() & values.keys():
+            if dict_vals[k] == values[k]: continue
+            raise ValueError(f'''Cannot resolve *A* (there could be more!) mismatched duplicate key from dict_vals and **values:
+                dict_vals[{k!r}] = {dict_vals[k]!r}
+                values    = {values[k]!r}''')
+        values |= dict_vals
         if len(values) == 0:
             self.logger.error(f'Instructed to set mass default of 0 values?')
             return
