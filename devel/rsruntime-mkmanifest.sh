d4d792a1e6e57fdfa5934a8fd9ea21fa5c207d8a
Shae <a.thomas.vc@gmail.com>
2023-11-22 22:27:49 -0800

    Added helper manifest devel scripts for _rsruntime/

---
+++ devel/rsruntime-mkmanifest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Generates a manifest for _rsruntime
+#  all arguments are supplied, even if they are simply the defaults, for documentation purposes
+
+./devel/mkmanifest.py make ./_rsruntime/ \
+    --output ./_rsruntime/MANIFEST.ini \
+    "RunServer Runtime Files" \
+    "http://0.0.0.0:8000/MANIFEST.ini" \
+    "http://0.0.0.0:8000/content/" \
+    --creator "Shae VanCleave" \
+    --creator-username "shae" \
+    --description "The actual code of RunServer" \
+    --key ./key.pyk \
+    --hash-algorithm "sha1" \
+    --system-info-level "full"
