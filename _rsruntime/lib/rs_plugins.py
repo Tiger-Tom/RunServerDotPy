172e1d3647874df355757c3e627bdf38373bf51a
Shae <a.thomas.vc@gmail.com>
2023-11-29 13:02:22 -0800

    Injected plugin 'self' into plugin module as 'this'

--- _rsruntime/lib/rs_plugins.py
+++ _rsruntime/lib/rs_plugins.py
@@ -30,6 +30,7 @@ class PluginManager:
             self.spec = iutil.spec_from_file_location(src.with_suffix('').as_posix().replace('/', '.'), self.source)
             self.module = iutil.module_from_spec(self.spec)
             self.module._ = RS._
+            self.module.this = self
             self.spec.loader.exec_module(self.module)
         def __getattr__(self, attr: str):
             if hasattr(self.module, attr):
