4b65e8a8bf85c77eae144fb3da2a2cefd4b39c2e
Shae <a.thomas.vc@gmail.com>
2023-11-30 10:15:18 -0800

    Implemented RunServer.py stub file

--- RunServer.py
+++ RunServer.py
@@ -1,23 +1,22 @@
 #!/bin/python3
 
+'''This is simply a stub file that fetches the bootstrapper (if it's missing) and executes it'''
+
 #> Imports
-import sys
+from pathlib import Path
 #</Imports
 
-#> Header
-def chainload_bootstrapper():
-    from _rsruntime.rs_BOOTSTRAP import Bootstrapper
-    bs = Bootstrapper()
-    bs.bootstrap()
-    return bs
-def fetch_bootstrapper():
-    ...
-#</Header
-
-#> Bootstrap >/
+#> Bootstrap**2 >/
 if __name__ == '__main__':
     try:
         from _rsruntime.rs_BOOTSTRAP import Bootstrapper
     except ImportError as e:
-        raise e
-    RS_Bootstrapper = chainload_bootstrapper()
+        import sys
+        from urllib import request
+        print(e, file=sys.stderr)
+        bpath = Path('_rsruntime/rs_BOOTSTRAP.py')
+        bpath.parent.mkdir(parents=True, exist_ok=True)
+        request.urlretrieve('https://raw.githubusercontent.com/Tiger-Tom/RunServerDotPy/v3.x.x/_rsruntime/rs_BOOTSTRAP.py', bpath)
+        from _rsruntime.rs_BOOTSTRAP import Bootstrapper
+    RS_Bootstrapper = Bootstrapper()
+    RS_Bootstrapper.bootstrap()
