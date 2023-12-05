04e5480ae62a784a629ddd384cd35281ce27e558
Shae <a.thomas.vc@gmail.com>
2023-12-04 21:16:56 -0800

    Implemented server JAr downloading

--- _rsruntime/lib/rs_mcmgr.py
+++ _rsruntime/lib/rs_mcmgr.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from urllib import request
 from collections import namedtuple
 from time import strptime
+from functools import cache
+from hashlib import sha1
 #</Imports
 
 # RunServer Module
@@ -20,18 +22,19 @@ class MinecraftManager:
 
     # Setup config
     Config.mass_set_default('minecraft/path/', base='./minecraft/', server_jar='server.jar')
-    Config.mass_set_default('minecraft/manager/',
+    Config.mass_set_default('minecraft/manager/dl',
                             auto_fetch_if_missing=True, auto_update=True,
+                            backup_replaced_jar=True,
                             version_manifest_url='https://launchermeta.mojang.com/mc/game/version_manifest.json',
                             bleeding_edge=False, only_snapshot=False,
-                            verify_download=True, fast_verify_download=False,
+                            hash_verify=True, size_verify=True, prompt_on_fail_verify=True,
                             time_fmt='%%Y-%%m-%%dT%%H:%%M:%%S%%z')
 
     def __init__(self):
         self.logger = RS.logger.getChild('MC')
     def init2(self):
-        if Config['minecraft/manager/auto_fetch_if_missing'] or Config['minecraft/manager/auto_update']:
-            try: self.setup_manifest()
+        if Config['minecraft/manager/dl/auto_fetch_if_missing'] or Config['minecraft/manager/dl/auto_update']:
+            try: self.versions = self.setup_manifest()
             except Exception as e:
                 self.logger.fatal(f'Could not setup Minecraft version manifest:\n{"".join(traceback.format_exception(e))}')
                 self.has_manifest = False
@@ -41,26 +44,70 @@ class MinecraftManager:
             if not self.has_manifest:
                 self.logger.irrec('Minecraft version manifest failed earlier; cannot continue')
                 raise FileNotFoundError(str(p))
-            if not Config['minecraft/manager/auto_fetch_if_missing']:
-                self.logger.irrec('Config minecraft/manager/auto_fetch_if_missing is false, cannot download!')
+            if not Config['minecraft/manager/dl/auto_fetch_if_missing']:
+                self.logger.irrec('Config minecraft/manager/dl/auto_fetch_if_missing is false, cannot download!')
                 raise FileNotFoundError(str(p))
-            self.missing_fetch()
+            self.install_update()
 
-    def setup_manifest(self):
-        self.logger.info(f'Fetching {Config["minecraft/manager/version_manifest_url"]}')
-        with request.urlopen(Config['minecraft/manager/version_manifest_url']) as r:
-            data = json.load(r)
-        tfmt = Config['minecraft/manager/time_fmt']
+    @staticmethod
+    def _fetch_url(url: str) -> bytes:
+        with request.urlopen(url) as r: return r.read()
+    @staticmethod
+    @cache
+    def _cached_fetch_url(url: str) -> bytes:
+        with request.urlopen(url) as r: return r.read()
+
+    def setup_manifest(self) -> 'VersionsType':
+        self.logger.info(f'Fetching {Config["minecraft/manager/dl/version_manifest_url"]}')
+        data = json.loads(self._fetch_url(Config['minecraft/manager/dl/version_manifest_url']).decode())
+        tfmt = Config['minecraft/manager/dl/time_fmt']
         versions = {v['id']: v | {
                 'time': strptime(v['time'], tfmt), '_time': v['time'],
                 'releaseTime': strptime(v['releaseTime'], tfmt), '_releaseTime': v['releaseTime']
             } for v in data['versions']}
-        self.versions = self.VersionsType(versions=versions,
+        return self.VersionsType(versions=versions,
             latest=max((versions[data['latest']['release']], versions[data['latest']['snapshot']]), key=lambda v: v['time']),
             latest_release=versions[data['latest']['release']],
             latest_snapshot=versions[data['latest']['snapshot']],
             releases = {k: v for k,v in versions.items() if v['type'] == 'release'},
             snapshots = {k: v for k,v in versions.items() if v['type'] == 'snapshot'},
         )
-    def missing_fetch(self):
-        ...
+
+    @property
+    def target(self) -> str:
+        return self.versions.latest if Config['minecraft/manager/dl/bleeding_edge'] else \
+               self.versions.latest_snapshot if Config['minecraft/manager/dl/only_snapshot'] else self.versions.latest_release
+    @property
+    def target_metadata(self) -> dict:
+        return json.loads(self._cached_fetch_url(self.target['url']).decode())
+
+    def install_update(self) -> bool:
+        jp = Path(Config['minecraft/path/base'], Config['minecraft/path/server_jar'])
+        if jp.exists():
+            self.logger.warning(f'Instructed to fetch server JAr, but it already exists at {jp}')
+            if Config['minecraft/manager/dl/backup_replaced_jar']:
+                op = jp.with_suffix(f'{jp.suffix}.old')
+                self.logger.warning(f'Copying {jp} to {op}')
+                op.write_bytes(jp.read_bytes())
+        tserver = self.target_metadata['downloads']['server']
+        self.logger.warning(f'Fetching server JAr of target version ID {self.target["id"]}')
+        data = self._fetch_url(tserver['url'])
+        if not self.verify_update(data, tserver['sha1'], tserver['size']):
+            return False
+        jp.parent.mkdir(parents=True, exist_ok=True)
+        jp.write_bytes(data)
+        return True
+    def verify_update(self, data: bytes, target_hash: str, target_size: int) -> bool:
+        failed_hash, failed_size = False, False
+        if (target_hash != (actual_hash := sha1(data).hexdigest())):
+            self.logger.fatal(f'The Minecraft server JAr failed hash verification:\n{target_hash=}\n{actual_hash=}')
+            failed_hash = True
+        if target_size != (actual_size := len(data)):
+            self.logger.fatal(f'The Minecraft server JAr failed size verification:\n{target_size=}\n{actual_size=}')
+            failed_size = True
+        if failed_hash or failed_size:
+            if input(f'The Minecraft server JAr failed '
+                     f'{"hash" if failed_hash else ""}{" and " if (failed_hash and failed_size) else ""}{"size" if failed_size else ""} '
+                     f'verification. Install it anyway? (y/N) >').lower() != 'y': return False
+            self.logger.warning('Installing anyway, proceed with caution')
+        return True
