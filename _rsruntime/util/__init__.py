81af01aaefab8ae8b5514110c33f0fb6be10adab
Shae <a.thomas.vc@gmail.com>
2023-12-05 12:31:06 -0800

    Alphabetically organized Util

--- _rsruntime/util/__init__.py
+++ _rsruntime/util/__init__.py
@@ -1,12 +1,12 @@
 #!/bin/python3
 
 #> Package >/
-__all__ = ('BetterPPrinter', 'fetch', 'INIBackedDict', 'JSONBackedDict', 'Hooks', 'Locker', 'PerfCounter', 'TimedLoadDebug', 'Timer')
+__all__ = ('BetterPPrinter', 'fetch', 'Hooks', 'INIBackedDict', 'JSONBackedDict', 'Locker', 'PerfCounter', 'TimedLoadDebug', 'Timer')
 
 from .betterprettyprinter import BetterPPrinter
-from .fbd import INIBackedDict, JSONBackedDict
 from . import fetch
 from .hooks import Hooks
+from .fbd import INIBackedDict, JSONBackedDict
 from . import locked_resource as Locker
 from .perfcounter import PerfCounter
 from .timed_load_debug import TimedLoadDebug
